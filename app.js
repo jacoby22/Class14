8577d60acc34104abc3756dd4eb2bc93ca587dc2
Nick Jacoby <nickjacoby22@gmail.com>
2016-07-02 17:58:13 -0700

    finished unit converter page

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
 var tracker = {
   conversionsByOrder: [2.54, 29.5735, 1.609344, 0.453592, 204.62262],
+  englishUnits: ['inch(es)', 'fluid ounce(s)', 'mile(s)','lb(s)', 'standard shit ton(s)'],
+  metricUnits: ['centimeter(s)', 'liter(s)', 'kilometer(s)','kg(s)', 'metric shit ton(s)'],
   returnValue: 0,
   userData: null,
   unitIndex: null,
   typeTo: null,
+  userInput: null,
   button: document.getElementById('Submit'),
+  clearButton: document.getElementById('clear'),
 
   convertToMetric: function(userData, unitId) {
-    console.log(userData);
-    console.log(unitId);
     var userValueArray = tracker.handleUserInput(userData, unitId);
     if (userValueArray[0] < tracker.conversionsByOrder.length - 1) {
-      tracker.returnValue = userValueArray[1] * this.conversionsByOrder[unitId];
+      tracker.returnValue = (userValueArray[1] * this.conversionsByOrder[unitId]).toFixed(3);
     } else {
-      tracker.returnValue = userValueArray[1] + this.conversionsByOrder[unitId];
+      tracker.returnValue = (userValueArray[1] + this.conversionsByOrder[unitId]).toFixed(3);
     }
     console.log(tracker.returnValue);
   },
   convertToEnglish: function(userData, unitId) {
     var userValueArray = tracker.handleUserInput(userData, unitId);
     if (userValueArray[0] < tracker.conversionsByOrder.length - 1) {
-      tracker.returnValue = userValueArray[1] / this.conversionsByOrder[unitId];
+      tracker.returnValue = (userValueArray[1] / this.conversionsByOrder[unitId]).toFixed(3);
     } else {
-      tracker.returnValue = userValueArray[1] - this.conversionsByOrder[unitId];
+      tracker.returnValue = (userValueArray[1] - this.conversionsByOrder[unitId]).toFixed(3);
     }
     console.log(tracker.returnValue);
   },
@@ -39,7 +41,6 @@ var tracker = {
     } else {
       tracker.typeTo = 'English';
     }
-    console.log(tracker.typeTo);
   },
   getUnitsElements: function() {
     for (var index in tracker.conversionsByOrder) {
@@ -50,18 +51,30 @@ var tracker = {
       }
     }
   },
-  handleSubmit: function() {
+  handleSubmit: function(event) {
+    event.preventDefault();
     tracker.getUnitsElements();
     tracker.getSystemElements();
-    var userInput = document.getElementById('userValue');
-    userData = userInput.value;
+    tracker.userInput = document.getElementById('userValue');
+    userData = tracker.userInput.value;
+    var ul = document.getElementById('list');
+    var li = document.createElement('li');
+    console.log(ul);
     if (tracker.typeTo === 'Metric') {
       tracker.convertToMetric(userData, tracker.unitIndex);
+      li.textContent = userData + ' ' + tracker.englishUnits[tracker.unitIndex] + ' equals ' + tracker.returnValue + ' ' + tracker.metricUnits[tracker.unitIndex];
+      ul.appendChild(li);
     } else {
       tracker.convertToEnglish(userData, tracker.unitIndex);
+      li.textContent = userData + ' ' + tracker.metricUnits[tracker.unitIndex] + ' equals ' + tracker.returnValue + ' ' + tracker.englishUnits[tracker.unitIndex];
+      ul.appendChild(li);
     }
+  },
+  clearList: function() {
+    var clearList = document.getElementById('list');
+    clearList.innerHTML = '';
   }
 };
 
-tracker.button.addEventListener('click', tracker.handleSubmit);
-tracker.getSystemElements();
+tracker.button.addEventListener('submit', tracker.handleSubmit);
+tracker.clearButton.addEventListener('click', tracker.clearList);
